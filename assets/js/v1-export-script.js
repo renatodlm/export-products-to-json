99c6eb2d72601acd5beb564a82db187e04514354
Renato Marques <renato@buildbox.com.br>
2024-09-21 13:45:28 -0300

    🔨

--- assets/js/v1-export-script.js
+++ assets/js/v1-export-script.js
@@ -2,135 +2,93 @@ var select2Options = {
    formatNoMatches: function () { return "Nada encontrado"; }
 }
 
-function buscarVeiculo(marca, modelo, ano, variants) {
+jQuery('.variant-component-2').ready(function () {
+   updateSelectMobile()
 
-   let results = []
-   for (let i = 0; i < variants.length; i++) {
-      const variant = variants[i];
-
-      if (marca && variant.attribute_pa_marca === marca) {
-         results.push(variant);
-      }
-
+   if (!isMobile()) {
+      jQuery('.select2').select2(select2Options);
+      jQuery('.select2-search input').attr('placeholder', 'buscar')
    }
-   results.sort(function (a, b) {
-      if (a.attribute_pa_modelo < b.attribute_pa_modelo) {
-         return -1;
+
+   const variants = jQuery('.variant-component-2').data('variants');
+
+   function populateSelect($select, options, placeholder) {
+      $select.empty();
+      $select.append('<option value="" disabled selected hidden>' + placeholder + '</option>');
+      options.forEach(function (option) {
+         $select.append('<option value="' + option.value + '">' + option.name + '</option>');
+      });
+      $select.prop('disabled', options.length === 0);
+   }
+
+   var $marcaSelect = jQuery('#variant-marca-2');
+   var $modeloSelect = jQuery('#variant-model-2');
+   var $anoSelect = jQuery('#variant-ano-2');
+
+   var rootKey = Object.keys(variants)[0];
+   var marcas = Object.values(variants[rootKey]);
+
+   populateSelect($marcaSelect, marcas, 'Marca');
+
+   $marcaSelect.on('change', function () {
+      var selectedMarca = $marcaSelect.val();
+      var marcaData = variants[rootKey][selectedMarca];
+
+      if (marcaData && marcaData.data && marcaData.data['attribute_pa_modelo']) {
+         var modelos = Object.values(marcaData.data['attribute_pa_modelo']);
+         populateSelect($modeloSelect, modelos, 'Modelo');
+      } else {
+         populateSelect($modeloSelect, [], 'Modelo');
       }
-      if (a.attribute_pa_modelo > b.attribute_pa_modelo) {
-         return 1;
-      }
-      return 0;
+
+      populateSelect($anoSelect, [], 'Ano');
+
+      jQuery('.variant-component-actions-2 button').prop("disabled", true)
+      jQuery('#variant-ano-2').val(null).trigger('change')
    });
 
-   let modelos = []
-   if (modelo) {
-      for (let i = 0; i < results.length; i++) {
-         const variant = results[i];
+   $modeloSelect.on('change', function () {
+      var selectedMarca = $marcaSelect.val();
+      var selectedModelo = $modeloSelect.val();
+      var modeloData = variants[rootKey][selectedMarca].data['attribute_pa_modelo'][selectedModelo];
 
-         if (modelo && variant.attribute_pa_modelo === modelo && variant.attribute_pa_marca === marca) {
-            modelos.push(variant);
-         }
-      }
+      if (modeloData && modeloData.data && modeloData.data['attribute_pa_ano']) {
+         var anos = Object.values(modeloData.data['attribute_pa_ano']);
+         populateSelect($anoSelect, anos, 'Ano');
 
-      results = modelos
-
-      results.sort(function (a, b) {
-         if (a.attribute_pa_ano < b.attribute_pa_ano) {
-            return -1;
-         }
-         if (a.attribute_pa_ano > b.attribute_pa_ano) {
-            return 1;
-         }
-         return 0;
-      });
-   }
-
-   let anos = []
-   if (ano) {
-      for (let i = 0; i < results.length; i++) {
-         const variant = results[i];
-
-         if (ano && variant.attribute_pa_ano === ano && variant.attribute_pa_modelo === modelo && variant.attribute_pa_marca === marca) {
-            anos.push(variant);
-         }
-      }
-      results = anos
-   }
-
-   return results;
-}
-
-function variantsToOptionsNoRepeat(variantsOld, key = 'attribute_pa_marca') {
-   let newOptions = []
-   for (let i = 0; i < variantsOld.length; i++) {
-      const variant = variantsOld[i];
-      if (!newOptions.includes(variant[key])) {
-         newOptions.push(variant[key])
-      }
-   }
-   return newOptions
-}
-
-
-var Select2Cascade = (function (window, $) {
-
-   function Select2Cascade(parent, child, getDataCallback, labels) {
-      var afterActions = [];
-
-      this.then = function (callback) {
-         afterActions.push(callback);
-         return this;
-      };
-
-      // parent.select2(select2Options).on("change", function (e) {
-      parent.on("change", function (e) {
-         child.prop("disabled", true);
-
-         var data = getDataCallback(jQuery(this).val());
-
-         var label = ''
-         if (jQuery(this).attr('id') === 'variant-marca') {
-            label = 'Modelo'
-         } else if (jQuery(this).attr('id') === 'variant-model') {
-            label = 'Ano'
-         }
-
-         updateSelect(child, data, label, labels);
-
-         afterActions.forEach(function (callback) {
-            callback(parent, child, data);
-         });
-      });
-   }
-
-   function updateSelect(select, data, label, labels) {
-      var newOptions = ''
-      if (!isMobile()) {
-         newOptions += '<option remove></option>';
+         jQuery('.variant-component-actions-2 button').prop("disabled", false)
+         jQuery('#variant-ano-2').val(null).trigger('change')
       } else {
+         populateSelect($anoSelect, [], 'Ano');
 
-         newOptions += '<option value="" disabled selected hidden>' + label + '</option>';
+         jQuery('.variant-component-actions-2 button').prop("disabled", true)
+         jQuery('#variant-ano-2').val(null).trigger('change')
       }
-      for (var i = 0; i < data.length; i++) {
-         newOptions += '<option value="' + data[i] + '">' + labels[data[i]] + '</option>';
+   });
+
+   $anoSelect.on('change', function () {
+      var selectedMarca = $marcaSelect.val();
+      var selectedModelo = $modeloSelect.val();
+      var selectedAno = $anoSelect.val();
+      var anoData = variants[rootKey][selectedMarca]
+         .data['attribute_pa_modelo'][selectedModelo]
+         .data['attribute_pa_ano'][selectedAno];
+
+      if (anoData && anoData.data) {
+         var products = [];
+         var attributeCor = anoData.data['attribute_pa_cor'];
+         if (attributeCor) {
+            Object.values(attributeCor).forEach(function (corOption) {
+               if (corOption.data && Array.isArray(corOption.data)) {
+                  corOption.data.forEach(function (product) {
+                     products.push(product.product_id);
+                  });
+               }
+            });
+         }
       }
-
-      if (!isMobile()) {
-         select.select2('destroy').html(newOptions).prop("disabled", false).select2(select2Options);
-      } else {
-         select.select2('destroy').html(newOptions).prop("disabled", false)
-      }
-   }
-
-   return Select2Cascade;
-
-})(window, $);
-
-
-function isMobile() {
-   return window.innerWidth < 1024
-}
+   });
+});
 
 function clearOptionns(el) {
    el.find('option[remove]').remove()
@@ -140,131 +98,19 @@ function clearOptionns(el) {
 
 function updateSelectMobile() {
    if (isMobile()) {
-      clearOptionns(jQuery('#variant-marca'))
-      clearOptionns(jQuery('#variant-model'))
-      clearOptionns(jQuery('#variant-ano'))
+      clearOptionns(jQuery('#variant-marca-2'))
+      clearOptionns(jQuery('#variant-model-2'))
+      clearOptionns(jQuery('#variant-ano-2'))
 
-      jQuery('#variant-marca').select2('destroy').append('<option value="" disabled selected hidden>Marca</option>')
+      jQuery('#variant-marca-2').select2('destroy').append('<option value="" disabled selected hidden>Marca</option>')
 
-      jQuery('#variant-model').select2('destroy').prop("disabled", true).append('<option value="" disabled selected hidden>Modelo</option>')
+      jQuery('#variant-model-2').select2('destroy').prop("disabled", true).append('<option value="" disabled selected hidden>Modelo</option>')
 
-      jQuery('#variant-ano').select2('destroy').prop("disabled", true).append('<option value="" disabled selected hidden>Ano</option>')
+      jQuery('#variant-ano-2').select2('destroy').prop("disabled", true).append('<option value="" disabled selected hidden>Ano</option>')
 
    }
 }
-// window.addEventListener('resize', updateSelectMobile)
 
-jQuery(document).ready(function () {
-   updateSelectMobile()
-
-   if (!isMobile()) {
-      jQuery('.select2').select2(select2Options);
-      jQuery('.select2-search input').attr('placeholder', 'buscar')
-   }
-
-   const variants = jQuery('.variant-component-2').data('variants')
-   console.log(variants);
-
-   variants.sort(function (a, b) {
-      if (a.attribute_pa_marca < b.attribute_pa_marca) {
-         return -1;
-      }
-      if (a.attribute_pa_marca > b.attribute_pa_marca) {
-         return 1;
-      }
-      return 0;
-   });
-
-   const newOptions = variantsToOptionsNoRepeat(variants)
-
-   for (let i = 0; i < newOptions.length; i++) {
-      const variant = newOptions[i];
-      var initbrands = new Option(labels[variant], variant, false, false);
-      jQuery("#variant-marca").append(initbrands)
-   }
-
-   jQuery("#variant-marca").trigger('change');
-
-   var marcaSelect = jQuery('#variant-marca');
-   var modeloSelect = jQuery('#variant-model');
-   var anoSelect = jQuery('#variant-ano');
-
-   modeloSelect.prop("disabled", true)
-   anoSelect.prop("disabled", true)
-
-   var cascadLoadingMarca = new Select2Cascade(marcaSelect, modeloSelect, function (valorSelecionado) {
-      return variantsToOptionsNoRepeat(buscarVeiculo(valorSelecionado, null, null, variants), 'attribute_pa_modelo');
-   }, labels);
-   cascadLoadingMarca.then(function (parent, child, data) {
-
-      if (!isMobile()) {
-         anoSelect.html('<option remove></option>')
-      } else {
-         anoSelect.html('<option value="" disabled selected hidden>Ano</option>')
-      }
-
-      if (marcaSelect.val()) {
-         marcaSelect.parent().addClass('selected')
-      } else {
-         marcaSelect.parent().removeClass('selected')
-      }
-
-      if (modeloSelect.val()) {
-         modeloSelect.parent().addClass('selected')
-      } else {
-         modeloSelect.parent().removeClass('selected')
-      }
-
-      jQuery('.variant-component-actions button').prop("disabled", false)
-      jQuery('#variant-ano').val(null).trigger('change')
-   });
-
-   var cascadLoadingModelo = new Select2Cascade(modeloSelect, anoSelect, function (valorSelecionado) {
-      return variantsToOptionsNoRepeat(buscarVeiculo(jQuery('#variant-marca').val(), valorSelecionado, null, variants), 'attribute_pa_ano');
-   }, labels);
-   cascadLoadingModelo.then(function (parent, child, data) {
-      if (modeloSelect.val()) {
-         modeloSelect.parent().addClass('selected')
-      } else {
-         modeloSelect.parent().removeClass('selected')
-      }
-   });
-
-   jQuery('#variant-ano').on('change', function () {
-      if (jQuery(this).val()) {
-         jQuery(this).parent().addClass('selected')
-         const message = labels[jQuery('#variant-marca').val()] + ' ' + labels[jQuery('#variant-model').val()] + ' ' + labels[jQuery(this).val()].toLowerCase()
-         jQuery('.variant-component-footer').show()
-         jQuery('.variant-component-footer').find('strong').text(message)
-      } else {
-         anoSelect.prop("disabled", true)
-         jQuery('.variant-component-footer').hide()
-         jQuery(this).parent().removeClass('selected')
-      }
-   })
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-   const originalForm = document.getElementById("variant-component-form");
-
-   originalForm.addEventListener("submit", function (e) {
-      e.preventDefault();
-
-      const newForm = document.createElement("form");
-      newForm.method = "get";
-      newForm.action = originalForm.action;
-
-      Array.from(originalForm.elements).forEach(function (element) {
-         if (element.name && element.value) {
-            const input = document.createElement("input");
-            input.type = "hidden";
-            input.name = element.name;
-            input.value = element.value;
-            newForm.appendChild(input);
-         }
-      });
-
-      document.body.appendChild(newForm);
-      newForm.submit();
-   });
-});
+function isMobile() {
+   return window.innerWidth < 1024
+}
--- export-products-to-json.php
+++ export-products-to-json.php
@@ -20,13 +20,13 @@ add_action('wp_enqueue_scripts',  'export_products_scripts', 100);
 
 function export_products_scripts()
 {
-   // wp_enqueue_style('variant-product-select2-style', plugin_dir_url(__FILE__) . 'assets/libs/select2/select2.css', array(), '1.0', 'all');
+   wp_enqueue_style('export-product-select2-style-2', plugin_dir_url(__FILE__) . 'assets/libs/select2/select2.css', array(), '1.0', 'all');
 
-   // wp_enqueue_style('variant-select-style2', plugin_dir_url(__FILE__) . 'assets/css/v1-export-style', array(), '1.0.0', 'all');
+   wp_enqueue_style('export-select-style2', plugin_dir_url(__FILE__) . 'assets/css/v1-export-style.css', array(), '1.0.0', 'all');
 
-   // wp_enqueue_script('variant-product-select2-script', plugin_dir_url(__FILE__) . 'assets/libs/select2/select2.min.js', array('jquery'), '1.0', true);
+   wp_enqueue_script('export-product-select2-script-2', plugin_dir_url(__FILE__) . 'assets/libs/select2/select2.min.js', array('jquery'), '1.0', true);
 
-   wp_enqueue_script('variant-select-script-2', plugin_dir_url(__FILE__) . 'assets/js/v1-export-script.js', array('jquery'), '1.0.0', true);
+   wp_enqueue_script('export-select-script-2', plugin_dir_url(__FILE__) . 'assets/js/v1-export-script.js', array('jquery'), '1.0.0', true);
 }
 
 class Export_Products_To_JSON
@@ -277,9 +277,8 @@ class Export_Products_To_JSON
          return;
       }
 
+      debug($atts['pid']);
       $file_path = plugin_dir_path(__FILE__) . 'products.json';
-
-      debug($file_path);
 
       if (!file_exists($file_path))
       {
@@ -302,44 +301,41 @@ class Export_Products_To_JSON
 
       ob_start();
    ?>
+
       <div class="variant-component-2" data-variants='<?php echo json_encode($json[$atts['pid']]); ?>'>
-         <form action="<?php echo \get_permalink($atts['pid']) ?>" id="variant-component-form" method="GET">
-            <div class="variant-component-container">
-               <div class="variant-component-header">
-                  <div class="variant-component-search">
-                     <i class="variant-component-icon-search">
+         <form action="<?php echo \get_permalink($atts['pid']) ?>" id="variant-component-form-2" method="GET">
+            <div class="variant-component-container-2">
+               <div class="variant-component-header-2">
+                  <div class="variant-component-search-2">
+                     <i class="variant-component-icon-search-2">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                            <path d="M9.3 3C5.83127 3 3 5.83127 3 9.3C3 12.7687 5.83127 15.6 9.3 15.6C10.8732 15.6 12.3105 15.0132 13.4168 14.0531L13.8 14.4363V15.6L19.2 21L21 19.2L15.6 13.8H14.4363L14.0531 13.4168C15.0132 12.3105 15.6 10.8732 15.6 9.3C15.6 5.83127 12.7687 3 9.3 3ZM9.3 4.8C11.7959 4.8 13.8 6.80406 13.8 9.3C13.8 11.7959 11.7959 13.8 9.3 13.8C6.80406 13.8 4.8 11.7959 4.8 9.3C4.8 6.80406 6.80406 4.8 9.3 4.8Z" fill="black" />
                         </svg>
                      </i>
                      <p><strong class="font-bold">Encontre o tapete</strong><br> para o seu veículo</p>
                   </div>
-                  <!-- <div class="variant-component-plate">
-                  <label for="vc-plate">buscar <strong class="font-medium">por placa</strong></label>
-                  <input id="vc-plate" class='toggle' type="checkbox" name='vc-plate' />
-               </div> -->
                </div>
-               <div class="variant-component-body">
-                  <div class="variant-component-option variant-marca">
-                     <select id="variant-marca" name="attribute_pa_marca" class="select2" placeholder="Marca">
+               <div class="variant-component-body-2">
+                  <div class="variant-component-option-2 variant-marca-2">
+                     <select id="variant-marca-2" name="attribute_pa_marca" class="select2" placeholder="Marca">
                         <option remove></option>
                      </select>
                   </div>
-                  <div class="variant-component-option variant-model">
-                     <select id="variant-model" name="attribute_pa_modelo" class="select2" placeholder="Modelo" disabled>
+                  <div class="variant-component-option-2 variant-model-2">
+                     <select id="variant-model-2" name="attribute_pa_modelo" class="select2" placeholder="Modelo" disabled>
                         <option remove></option>
                      </select>
                   </div>
-                  <div class="variant-component-option variant-ano">
-                     <select id="variant-ano" name="attribute_pa_ano" class="select2" placeholder="Ano" disabled>
+                  <div class="variant-component-option-2 variant-ano-2">
+                     <select id="variant-ano-2" name="attribute_pa_ano" class="select2" placeholder="Ano" disabled>
                         <option remove></option>
                      </select>
                   </div>
-                  <div class="variant-component-actions">
+                  <div class="variant-component-actions-2">
                      <button disabled>Ver</button>
                   </div>
                </div>
-               <div class="variant-component-footer" style="display: none;">
+               <div class="variant-component-footer-2" style="display: none;">
                   <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                      <g clip-path="url(#clip0_2012_2240)">
                         <path d="M7.99999 1.33331C4.31999 1.33331 1.33333 4.31998 1.33333 7.99998C1.33333 11.68 4.31999 14.6666 7.99999 14.6666C11.68 14.6666 14.6667 11.68 14.6667 7.99998C14.6667 4.31998 11.68 1.33331 7.99999 1.33331ZM6.66666 11.3333L3.33333 7.99998L4.27333 7.05998L6.66666 9.44665L11.7267 4.38665L12.6667 5.33331L6.66666 11.3333Z" fill="#48BA12" />
@@ -350,7 +346,7 @@ class Export_Products_To_JSON
                         </clipPath>
                      </defs>
                   </svg>
-                  <p class="variant-component-message">
+                  <p class="variant-component-message-2">
                      Perfeito para <strong class="font-semibold"></strong>
                   </p>
                </div>
@@ -358,7 +354,6 @@ class Export_Products_To_JSON
          </form>
       </div>
 <?php
-      debug('ue');
       $content = ob_get_clean();
 
       return $content;
